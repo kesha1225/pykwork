7d299ba24d0b77d014b64ad4bc52c18581215231
kesha1225 <samedov03@mail.ru>
2020-05-09 01:56:26 +0300

    fix category

--- kwork/types/category.py
+++ kwork/types/category.py
@@ -3,7 +3,6 @@ import typing
 from pydantic import BaseModel, validator
 
 
-
 class Subcategory(BaseModel):
     id: int
     name: str
@@ -18,7 +17,7 @@ class Category(BaseModel):
     id: int
     name: str
     description: typing.Optional[str]
-    subcategories: typing.List[dict]
+    subcategories: typing.List[typing.Union[dict, Subcategory]]
 
     _normalize_subcategories = validator("subcategories", allow_reuse=True)(
         normalize_subcategories
